6ac9df015697a395dd4ae472818940692be98f2a
Shubham More <shubhammore1796@gmail.com>
2021-03-11 16:30:28 +0530

    sportizen

--- src/app/@shared/pipe/capitalize-each-word.pipe.ts
+++ src/app/@shared/pipe/capitalize-each-word.pipe.ts
@@ -5,8 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CapitalizeEachWordPipe implements PipeTransform {
   transform(name: string): string {
-    const nameArr: String[] = name.split(' ');
-    const newNameArr: String[] = [];
+    if (!name) {
+      return '';
+    }
+    const nameArr: string[] = name.split(' ');
+    const newNameArr: string[] = [];
     for (let n of nameArr) {
       const newName: string = n.charAt(0).toUpperCase() + n.slice(1);
       newNameArr.push(newName);
--- src/app/dashboard/home/search-user/all-users/all-users.component.ts
+++ src/app/dashboard/home/search-user/all-users/all-users.component.ts
@@ -1,3 +1,4 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ConnectionService } from './../../../../services/connection.service';
@@ -10,7 +11,11 @@ import { ConnectionService } from './../../../../services/connection.service';
 export class AllUsersComponent implements OnInit {
   searchResults: any;
 
-  constructor(private route: ActivatedRoute, private connectionService: ConnectionService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private connectionService: ConnectionService,
+    public snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((param: Params) => {
@@ -21,5 +26,23 @@ export class AllUsersComponent implements OnInit {
         });
       }
     });
+  }
+
+  followUnfollow(name: string, sportizenId: string, connectionStatus: string, i: number) {
+    if (connectionStatus === 'following') {
+      this.connectionService.unfollowConnection(sportizenId).subscribe((res: any) => {
+        this.searchResults[i].connectionStatus = 'not-connected';
+        this.snackBar.open(`You unfollowed  ${name}`, null, {
+          duration: 2000,
+        });
+      });
+    } else {
+      this.connectionService.sendConnectionRequest(sportizenId).subscribe((res: any) => {
+        this.searchResults[i].connectionStatus = res.status;
+        this.snackBar.open(`You are now following ${name}`, null, {
+          duration: 2000,
+        });
+      });
+    }
   }
 }
